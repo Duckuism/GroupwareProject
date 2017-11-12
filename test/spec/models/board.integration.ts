5d86e30007d271f2c1fc0b541762b897fef974a7
한유덕 <Duck@MacBook-Pro.local>
2017-11-12 16:12:57 +0900

    댓글 CRUD 완료

--- test/spec/models/board.integration.ts
+++ test/spec/models/board.integration.ts
@@ -2,6 +2,7 @@ import {expect} from "chai";
 import {sequelize} from "../../../src/models/index";
 import Employee from "../../../src/models/domain/employee";
 import Board from "../../../src/models/domain/board";
+import {where} from "sequelize";
 
 
 describe("[integration] 게시판 모델을 테스트 중입니다.",function(){
@@ -53,37 +54,43 @@ describe("[integration] 게시판 모델을 테스트 중입니다.",function(){
 
   it('게시글을 변경한다.', function(done){
     let givenBoard = {title:'글 제목1', content:'글 내용1', writer:'글 작성자1'};
-    let getGivenBoard = function(){return givenBoard;};
     let updateBoard = {title:'변경된 글 제목1', content:'변경된 글 내용1', writer:'변경된 글 작성자1'};
-    let setGivenBoard = function(a){return givenBoard = a;};
 
     save(givenBoard, (saveBoard: Board)=>{
-      Board.findOne<Board>({where:{title: '글 제목1'}})
+      saveBoard.update({title:'변경된 글 제목1', content:'변경된 글 내용1', writer:'변경된 글 작성자1'}, {where:{title:'글 제목1'}})
         .then((board: Board) => {
-          board.title = updateBoard.title;
-          board.content = updateBoard.content;
-          console.log(board);
+          expect(board.title).to.be.equal(updateBoard.title);
+          expect(board.content).to.be.equal(updateBoard.content);
           done();
       });
-      // Board.findOne<Board>({where:{title: '변경된 글 제목1'}})
-      //   .then((board: Board)=>{
-      //   expect(board.title).to.equal(givenBoard.title);
-      //   });
     });
-
   });
 
-  it.only('게시글을 삭제한다.',function(done){
-    let givenBoard = {title:'글 제목1', content:'글 내용1', writer:'글 작성자1'};
-
-    save(givenBoard, (saveBoard: Board) =>{
-      Board.findOne<Board>({where:{title: '글 제목1'}})
-        .then((board: Board) => {
-            board.destroy();
-            console.log(board);
-            done();
-          });
+  // it('게시글을 삭제한다.',function(done){
+  //   let givenBoard = {title:'글 제목1', content:'글 내용1', writer:'글 작성자1'};
+  //
+  //   save(givenBoard, (saveBoard: Board) =>{
+  //       saveBoard.destroy({where:{title:'글 제목1'}})
+  //       .then((board: Board) => {
+  //           expect(board.title).to.be.equal(undefined);
+  //           done();
+  //         });
+  //     });
+  //   });
+  it('제목에 맞는 게싯글을 삭제', (done: Function) => {
+    // given
+    let givenBoard = {title: '글 제목1', content: '글 내용1', writer:'글 작성자1'};
+    // when
+    save(givenBoard, (saveBoard: Board) => {
+      // then
+      //일단 Board 모델에서 조건에 맞는 row를 찾고 익명함수 실행
+      Board.destroy({where:{title:'글 제목1'}}).then(()=> {
+        //다시 Board 모델에서 destroy한 객체와 같은 조건으로 객체를 찾아서 null이랑 비교.
+        Board.findOne<Board>({where:{title:'게시글 제목'}}).then((findedBoard:Board)=>{
+          expect(findedBoard).to.be.eql(null);
+          done();
+        });
       });
     });
-
+  });
 });
--- test/spec/models/comment.integration.ts
+++ test/spec/models/comment.integration.ts
@@ -2,88 +2,93 @@ import {expect} from "chai";
 import {sequelize} from "../../../src/models/index";
 import Employee from "../../../src/models/domain/employee";
 import Board from "../../../src/models/domain/board";
+import Comment from "../../../src/models/domain/comment";
 
 
-describe("[integration] 게시판 모델을 테스트 중입니다.",function(){
+describe("[integration] 댓글 모델을 테스트 중입니다.",function(){
 
+  //비동기를 위해 sync를 맞추는 promise구조 hook작업이군
   before((done:Function) => {
     sequelize.sync().then(() => {
+      //싱크가 잘맞으면 성공이고,
       done();
     }).catch((error: Error) => {
+      //싱크가 안맞으면 에러를 뿜습니다.
       done(error);
     });
   });
 
-  const cleanUp2 = (cb) => Board.destroy({where: {}, truncate: true}).then(() => cb());
+  // 그럼 테스트를 시작하기 전에 깨끗하게 모든 걸 지워 보실까? truncate : table로우를 다 조져서 지우는 데이터 무결성 파괴의 주범.
+  const cleanUp = (cb) => Comment.destroy({where: {}, truncate: true}).then(() => cb());
 
+  //모든 테스트를 시작하기 전에 초기화
   beforeEach((done: Function)=>{
-    cleanUp2(()=>done());
+    cleanUp(()=>done());
   });
 
+  //주어진 객체를 저장하는 메서드
   const save = (given, cb) => {
-    const board = new Board(given);
-    board.save()
-      .then((saveBoard: Board) => {
-        cb(saveBoard);
+    const comment = new Comment(given);
+    comment.save()
+      .then((saveComment: Comment) => {
+        cb(saveComment);
       });
   };
 
-  it('게시글을 추가한다', function(done){
-    let givenBoard = {title:'글 제목1', content:'글 내용1', writer:'글 작성자1'};
+  it('댓글을 추가한다', function(done){
+    let givenComment = { content:'글 내용1', writer:'글 작성자1'};
 
-    save(givenBoard, (saveBoard: Board) => {
-      expect(saveBoard.title).to.be.eql(givenBoard.title);
-      expect(saveBoard.content).to.be.eql(givenBoard.content);
-      expect(saveBoard.writer).to.be.eql(givenBoard.writer);
+    save(givenComment, (saveComment: Comment) => {
+      expect(saveComment.content).to.be.eql(givenComment.content);
+      expect(saveComment.writer).to.be.eql(givenComment.writer);
       done();
     });
   });
 
-  it('게시글을 조회한다', function(done){
-    let givenBoard = {title:'글 제목1', content:'글 내용1', writer:'글 작성자1'};
+  it('댓글을 조회한다', function(done){
+    let givenComment = { content:'글 내용1', writer:'글 작성자1'};
 
-    save(givenBoard, (saveBoard: Board)=>{
-      Board.findOne<Board>({where:{title: '글 제목1'}})
-        .then((board: Board) => {
-          expect(board.title).to.be.equal(givenBoard.title);
+    save(givenComment, (saveComment: Comment)=>{
+      Comment.findOne<Comment>({where:{writer:'글 작성자1'}})
+        .then((board: Comment) => {
+          expect(board.writer).to.be.equal(givenComment.writer);
           done();
         });
     });
   });
 
-  it('게시글을 변경한다.', function(done){
-    let givenBoard = {title:'글 제목1', content:'글 내용1', writer:'글 작성자1'};
-    let getGivenBoard = function(){return givenBoard;};
-    let updateBoard = {title:'변경된 글 제목1', content:'변경된 글 내용1', writer:'변경된 글 작성자1'};
-    let setGivenBoard = function(a){return givenBoard = a;};
+  it('댓글을 변경한다.', function(done){
+    let givenComment = {content:'글 내용1', writer:'글 작성자1'};
+    let updateComment = {content:'변경된 글 내용1', writer:'변경된 글 작성자1'};
 
-    save(givenBoard, (saveBoard: Board)=>{
-      Board.findOne<Board>({where:{title: '글 제목1'}})
-        .then((board: Board) => {
-          board.title = updateBoard.title;
-          board.content = updateBoard.content;
-          console.log(board);
-          done();
-      });
-      // Board.findOne<Board>({where:{title: '변경된 글 제목1'}})
-      //   .then((board: Board)=>{
-      //   expect(board.title).to.equal(givenBoard.title);
-      //   });
-    });
-
-  });
-
-  it.only('게시글을 삭제한다.',function(done){
-    let givenBoard = {title:'글 제목1', content:'글 내용1', writer:'글 작성자1'};
-
-    save(givenBoard, (saveBoard: Board) =>{
-      Board.findOne<Board>({where:{title: '글 제목1'}})
-        .then((board: Board) => {
-            board.destroy();
-            console.log(board);
+    save(givenComment, (saveComment: Comment)=>{
+      Comment.update({content:'변경된 글 내용1', writer:'변경된 글 작성자1'},{where:{writer:'글 작성자1'}}).then(()=>{
+        //findOne으로 찾은 Comment 모델 안의 객체를 comment라는 변수에 저장해서 함수의 파라미터로 넘긴다. 이걸 이해하기 힘들었네.
+        Comment.findOne<Comment>({where: {writer:'변경된 글 작성자1'}})
+          .then((comment: Comment) => {
+            //댓글은 내용과 작성자만 만들어놨으므로 title 해놓으면 에러난다.
+            expect(comment.content).to.be.equal(updateComment.content);
+            expect(comment.writer).to.be.equal(updateComment.writer);
             done();
           });
       });
     });
+  });
+
+  it.only('댓글을 삭제한다.',function(done){
+    let givenComment = {content:'글 내용1', writer:'글 작성자1'};
+
+    save(givenComment, (saveComment: Comment) =>{
+      Comment.destroy({where:{writer:'글 작성자1'}}).then(()=>{
+        Comment.findOne<Comment>({where:{content:'글 내용1', writer:'글 작성자1'}}).then((comment: Comment)=>{
+          expect(comment).to.be.equal(null);
+          done();
+        });
+      });
+
+
+
+    });
+  });
 
 });
